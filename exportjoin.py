15a853d282a37c12f04803df27d3170b9cda9803
jldaggett <daggettjuliana@gmail.com>
2025-09-26 15:37:14 -0500

    exportjoin.py

--- exportjoin.py
+++ exportjoin.py
@@ -1,20 +1,26 @@
-#IMPORT PACKAGES
+#IMPORTS PACKAGES
 import arcpy
 import os
 import sys
 import tempfile
 from datetime import datetime
-# GET INPUT TABLE PARAMETER FROM MODEL BUILDER (JOINED TABLE)
+
+
+#GETS INPUT TABLE PARAMETER FROM MODEL BUILDER (JOINED TABLE)
 input_table = arcpy.GetParameterAsText(0)
+
 # GET OUTPUT LOCATION OF EXPORTED JOIN FILE
 network_export_file = arcpy.GetParameterAsText(1)
-#DEFINE VARIABLES
+
+#DEFINES VARIABLES
 current_year = datetime.now().strftime("%Y")
 today_date = datetime.now().strftime("%m%d%Y")
-#SET UP FOLDER PATHS
+
+#SETS UP FOLDER PATHS
 network_export_folder = rf"\\MCGIS\Data\MapData\Parcel\{current_year}\{today_date}"
 network_export_file = os.path.join(network_export_folder, "TAXPARCEL")
-#EXPORT THE JOINED TABLE
+
+#EXPORTS JOINED TABLE
 try:
     arcpy.AddMessage("Exporting Joined Table...")
     arcpy.conversion.ExportFeatures(
@@ -30,6 +36,6 @@ except Exception as e:
     arcpy.AddError(f"Error during export: {e}")
     sys.exit()
     
-#SET THE OUTPUT PARAMETER
+#SETS OUTPUT PARAMETER
 arcpy.SetParameterAsText(1, network_export_file)
 arcpy.AddMessage(f"Export complete! Output saved to: {network_export_file}")
